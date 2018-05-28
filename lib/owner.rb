699486d3708cf02502e0d8f4519a3188fd98aa62
Conor Hamilton <conhamil19@gmail.com>
2018-05-28 04:31:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,12 +34,12 @@ class Owner
   
    def buy_cat(name)
     new_cat = Cat.new(name)
-    @pets[:cat] << new_cat
+    @pets[:cats] << new_cat
   end
   
-   def buy_fish(name)
-    new_fish = Fish.new(name)
-    @pets[:fishes] << new_fish
+   def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
   end
   
 end 
