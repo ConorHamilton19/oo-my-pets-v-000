b9a45130a1590c4fc3e4569c3dd135e324b0856d
Conor Hamilton <conhamil19@gmail.com>
2018-05-28 04:31:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,4 +32,14 @@ class Owner
     @pets[:fishes] << new_fish
   end
   
+   def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cat] << new_cat
+  end
+  
+   def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
+  
 end 
